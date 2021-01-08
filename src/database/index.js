39b7e1a7ab2fa2a4d487602450a64846c930466d
Preduh <pedro.lucas5860@gmail.com>
2021-01-08 17:23:00 -0300

    no env

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+require('dotenv').config({ path: '.env' })
 
 mongoose.connect(process.env.MONGO_URL, { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = global.Promise
